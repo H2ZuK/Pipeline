67cb424b74a2e22cb523afe4777cb3291707287b
H2ZuK <147606534+H2ZuK@users.noreply.github.com>
2025-01-13 15:47:14 +0800

    Update otp.js

--- otp.js
+++ otp.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-const auth = getAuth(app);
+
 
 const verify = document.getElementById('verify');
 verify.addEventListener("click", function (event){
