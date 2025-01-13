fbf8bc08eeb94e62d3d4326e00e9b36d3fbdd4a3
H2ZuK <147606534+H2ZuK@users.noreply.github.com>
2025-01-13 15:47:01 +0800

    Update otp.js

--- otp.js
+++ otp.js
@@ -1,19 +1,4 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDggrd5k_fx2RXEb67Z-8dGwLbSQCqdKgU",
-  authDomain: "pipeline-76272.firebaseapp.com",
-  projectId: "pipeline-76272",
-  storageBucket: "pipeline-76272.firebasestorage.app",
-  messagingSenderId: "732061879569",
-  appId: "1:732061879569:web:041d0e90d3eb09024e10b0"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 const verify = document.getElementById('verify');
@@ -23,9 +8,9 @@ verify.addEventListener("click", function (event){
   const code = document.getElementById('otp').value;
   
   if (window.confirmationResult) {
-    window.confirmationResult.confirm(code).then((result) => {
+    window.confirmationResult.confirm(code).then(() => {
       // User signed in successfully.
-      const user = result.user;
+      
       alert("Phone number verified successfully!");
       window.location.href = "homepage.html";
     }).catch((error) => {
